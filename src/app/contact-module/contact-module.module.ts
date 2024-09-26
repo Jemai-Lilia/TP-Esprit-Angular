0c69733f1ebeb13653e4957af2a7c609ac3e58ee
Lilia_Jemai <@DevHarmony>
2024-09-26 14:02:39 +0100

    Component+Module

---
+++ src/app/contact-module/contact-module.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ContactModuleRoutingModule } from './contact-module-routing.module';
+import { ContactComponent } from '../contact/contact.component';
+
+
+@NgModule({
+  declarations: [
+    ContactComponent
+  ],
+  imports: [
+    CommonModule,
+    ContactModuleRoutingModule
+  ]
+})
+export class ContactModuleModule { }
